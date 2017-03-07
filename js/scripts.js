73d761c3c0ac0f278d417df7188f43f1b5eb1093
liam stabeno and nathaniel meyer <liam@>
2017-03-07 14:34:12 -0800

    Refactor UI and switchTurns to dry actions and buttons

--- js/scripts.js
+++ js/scripts.js
@@ -7,14 +7,16 @@ var switchTurns = function() {
   passivePlayer = placeHolder;
   console.log(activePlayer);
   console.log(passivePlayer);
+  toggleButtons();
 };
 
 var diceRoller = function(sides) {
   return Math.ceil(Math.random() * sides);
 };
 
-function Character(charName, hP, attackStat, attackModifier, defenseStat, defenseModifier){
+function Character(charName, charImgUrl, hP, attackStat, attackModifier, defenseStat, defenseModifier){
   this.charName = charName;
+  this.charImgUrl = charImgUrl
   this.hP = hP;
   this.attackStat = attackStat;
   this.attackModifier = attackModifier;
@@ -39,55 +41,101 @@ Character.prototype.outcome = function(c1Attack, c2Defense){
 
 var characters = [];
 
-var max = new Character("Max", 100, 10, 10, 10);
+var max = new Character("Max", "img/player1.jpg", 100, 10, 10, 10);
 characters.push(max);
-var dick = new Character("Dick", 100, 10, 5, 15);
+var dick = new Character("Dick", "img/player2.jpg", 100, 10, 5, 15);
 characters.push(dick);
-activePlayer = characters[0];
-passivePlayer = characters[1];
 
-function attack() {
+var setInitialTurnOrder = function() {
+  activePlayer = characters[0];
+  passivePlayer = characters[1];
+};
+setInitialTurnOrder();
+
+function attackButton() {
   passivePlayer.outcome(activePlayer.attack(), passivePlayer.defense());
-  $("#p2Status").text(characters[1].hP);
+  $("#p1Status").text(activePlayer.hP);
   switchTurns();
 };
 
 function defend() {
   activePlayer.defenseModifier = (activePlayer.defenseStat)/3;
   switchTurns();
-
-
-
 }
 // function p1Defend() {
 //   max.defense();
 //   $("#p1Status").text("P1 is choosing to defend.");
 // };
-function p2Attack() {
+function attack() {
   characters[0].outcome(characters[1].attack(), characters[0].defense());
   $("#p1Status").text(characters[0].hP);
   switchTurns();
 };
+// function attack() {
+//   passivePlayer.outcome(activePlayer.attack(), passivePlayer.defense());
+//   $("#p2Status").text(characters[1].hP);
+//   switchTurns();
+// };
+
 // function p2Defend() {
 //   dick.defense();
 //   $("#p2Status").text("P2 is choosing to defend.");
 // };
 
+var toggleButtons = function() {
+  $(".btn").each(function() {
+    if (this.hasAttribute("disabled")) {
+      $(this).prop("disabled", false);
+    } else {
+      $(this).prop("disabled", true);
+    }
+  });
+};
+
 //Front-end logic
 $(document).ready(function() {
+
+var populatePlayerInterface = function(player) {
+  $("div#playerInterface").append('<div class="col-md-6">' +
+                                      '<img src="' +
+                                      player.charImgUrl +
+                                      '" alt="' +
+                                      player.charName +
+                                      '" id="' +
+                                      player.charName +
+                                      'Image">' +
+                                      '<h2>' +
+                                      player.charName +
+                                      '</h2>' +
+                                    '</div>'
+                                  );
+  $("div#playerStatus").append('<div class="col-md-6">' +
+                                  '<p id="' +
+                                  player.charName +
+                                  'Status">' +
+                                  '</p>' +
+                                '</div>'
+                              );
+  $("div#playerControls").append('<div class="col-md-6">' +
+                                    '<button class="btn attack" type="click">Attack</button>' +
+                                    '<button class="btn defend" type="click">Defend</button>' +
+                                  '</div>'
+                                );
+};
+
+populatePlayerInterface(activePlayer);
+switchTurns();
+populatePlayerInterface(activePlayer);
+switchTurns();
+
+
   $("#p1Name").text((characters[0].charName));
   $("#p2Name").text((characters[1].charName));
 
-  $("#p1Attack").click(function() {
-    p1Attack();
+  $(".attack").click(function() {
+    attackButton();
   });
-  $("#p1Defend").click(function() {
-    p1Defend();
-  });
-  $("#p2Attack").click(function() {
-    p2Attack();
-  });
-  $("#p2Defend").click(function() {
-    p2Defend();
+  $(".defend").click(function() {
+    defend();
   });
 });
