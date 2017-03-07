3f6b3da54e37ed1697dd7ab40e9ca0303e70776d
Evan Bishop and Shane Stafford <shane@>
2017-03-07 10:02:13 -0800

    merge back-end changes with front-end

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,18 @@
 //Back-end logic
-function Character(charName, hP, attackStat, defenseStat){
+var diceRoller = function(sides) {
+  return Math.ceil(Math.random() * sides);
+};
+
+function Character(charName, hP, attackStat, attackModifier, defenseStat){
   this.charName = charName;
   this.hP = hP;
   this.attackStat = attackStat;
+  this.attackModifier = attackModifier;
   this.defenseStat = defenseStat;
 };
 
 Character.prototype.attack = function(){
-  return this.attackStat;
+  return this.attackStat + diceRoller(this.attackModifier);
 };
 
 Character.prototype.defense = function(){
@@ -23,7 +28,13 @@ Character.prototype.outcome = function(c1Attack, c2Defense){
   }
 };
 
-//Front-end logic
+var characters = [];
+
+var newCharacter = new Character("Max", 100, 10, 10, 10);
+characters.push(newCharacter);
+var newCharacter = new Character("Dick", 100, 10, 5, 15);
+characters.push(newCharacter);
+
 function p1Attack() {
   player1.attack();
   player2.outcome();
@@ -43,6 +54,7 @@ function p2Defend() {
   $("#p2Status").text("P2 is choosing to defend.");
 };
 
+//Front-end logic
 $(document).ready(function() {
 
   $("#p1Attack").click(function() {
