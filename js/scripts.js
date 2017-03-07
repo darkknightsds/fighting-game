60fd056380a85a66f60156178b66ef57e8cefceb
Evan Bishop and Shane Stafford <shane@>
2017-03-07 09:41:12 -0800

    reorder front and back end

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,27 @@
+//Back-end logic
+function Character(charName, hP, attackStat, defenseStat){
+  this.charName = charName;
+  this.hP = hP;
+  this.attackStat = attackStat;
+  this.defenseStat = defenseStat;
+};
+
+Character.prototype.attack = function(){
+  return this.attackStat;
+};
+
+Character.prototype.defense = function(){
+  return this.defenseStat;
+};
+
+Character.prototype.outcome = function(c1Attack, c2Defense){
+  if (c1Attack > c2Defense) {
+    this.hP -= (c1Attack - c2Defense);
+  }
+  if (this.hP < 0) {
+    this.death();
+  }
+};
 
 //Front-end logic
 function p1Attack {
@@ -39,33 +63,3 @@ $(document).ready(function() {
   });
 
 });
-//back-end
-
-function Character(charName, hP, attackStat, defenseStat){
-  this.charName = charName;
-  this.hP = hP;
-  this.attackStat = attackStat;
-  this.defenseStat = defenseStat;
-};
-
-Character.prototype.attack = function(){
-  return this.attackStat;
-};
-
-Character.prototype.defense = function(){
-  return this.defenseStat;
-};
-
-Character.prototype.outcome = function(c1Attack, c2Defense){
-  if (c1Attack > c2Defense) {
-    this.hP -= (c1Attack - c2Defense);
-  }
-  if (this.hP < 0) {
-    this.death();
-  }
-};
-
-
-
-
-//front-end
